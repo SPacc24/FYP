73f9c048c89b3b8bf8d323c469f312c5a4a75fca
Nitroxyl <CEDRICONG.24@ichat.sp.edu.sg>
2026-04-27 13:43:49 +0800

    will continue ltr

--- project/app.py
+++ project/app.py
@@ -2,6 +2,13 @@ from flask import Flask, request, jsonify
 from caldera.api_client import CalderaClient
 from caldera.operation_manager import OperationManager
 import os
+import atexit
+import signal
+import sys
+from utils.cleanup import full_cleanup
+
+# register cleanup on exit
+atexit.register(full_cleanup)
 
 app = Flask(__name__)
 
@@ -9,6 +16,13 @@ app = Flask(__name__)
 caldera_url = os.getenv("CALDERA_URL", "http://localhost:8888")
 caldera_api_key = os.getenv("CALDERA_API_KEY", "ADMIN123")
 caldera_client = CalderaClient(caldera_url, caldera_api_key)
+
+def handle_exit(signum, frame):
+    full_cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_exit)   # Ctrl+C
+signal.signal(signal.SIGTERM, handle_exit)  # kill
 
 @app.route("/caldera/status")
 def caldera_status():
